13613835d799f17d881797dd149492806d3899cd
Taylor Wise <taylor2julie@gmail.com>
2020-05-15 22:57:07 -0500

    I believe I have met MVP

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,35 @@
 import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import { render, fireEvent } from "@testing-library/react";
+import ContactForm from "./components/ContactForm";
+import { act } from "react-dom/test-utils";
 
 test("renders App without crashing", () => {
-  render(<App />);
+  const { getByTestId } = render(<ContactForm />);
+  const form = getByTestId("form");
+  expect(form).toBeInTheDocument();
+});
+
+await test("Testing Form input", () => {
+  
+  const { getByTestId } = render(<ContactForm />);
+
+  const firstNameInput = getByTestId("firstName");
+  const lastNameInput = getByTestId("lastName");
+  const emailInput = getByTestId("email");
+  const messageInput = getByTestId("message");
+  const button = getByTestId("submitbtn");
+
+  fireEvent.change(firstNameInput, { target: { value: "Taylor" } });
+  fireEvent.change(lastNameInput, { target: { value: "Wise" } });
+  fireEvent.change(emailInput, { target: { value: "taylorW@TaylorW.com" } });
+  fireEvent.change(messageInput, { target: { value: "Testing Message" } });
+
+  expect(firstNameInput.value).toBe("Taylor");
+  expect(lastNameInput.value).toBe("Wise");
+  expect(emailInput.value).toBe("taylorW@TaylorW.com");
+  expect(messageInput.value).toBe("Testing Message");
+
+  fireEvent.click(button);
+
+  expect(getByTestId("preTag")).toBeInTheDocument();
 });
